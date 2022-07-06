465a3e79551de2a4779eabc275647d268b3e4512
okputu julius <juliusstan10@gmail.com>
2022-07-06 19:51:12 +0100

    feat:add blog app

---
+++ blog/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    author=forms.CharField(max_length=60,widget=forms.TextInput(
+        attrs={
+        'class':'form_control',
+        'placeholder':'your name'
+        }
+    )
+    )
+    body=forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class':'form-control',
+            'placeholder':'leave a comment!'
+
+        }
+    )
+
+    )
